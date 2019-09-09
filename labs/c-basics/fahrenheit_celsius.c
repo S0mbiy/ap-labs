96f23794778fd76cf574e7a0b67db655a3363b83
Sergio <A01228085@itesm.mx>
2019-09-08 19:05:50 -0500

    Fahrenheir to celsius

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
-
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
+#include <stdlib.h>
 
 /* print Fahrenheit-Celsius table */
 
-int main()
+int main(int argc, char **argv)
 {
+  if(argc == 4){
     int fahr;
-
+    int LOWER = atoi(argv[1]);
+    int UPPER = atoi(argv[2]);
+    int STEP = atoi(argv[3]);
+    if (STEP < 1 || UPPER < LOWER)
+      return 2;
     for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
-    return 0;
+	     printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+  }
+  else if (argc == 2){
+    int fahr = atoi(argv[1]);
+    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+  }
+  else
+    return 1;
+  return 0;
 }
