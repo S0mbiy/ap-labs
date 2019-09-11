0740f3ff00513f5f47dda7b3a7f19219cb3badb5
Sergio <A01228085@itesm.mx>
2019-09-10 21:22:28 -0500

    go maps slices exercises

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	m := make(map[string]int)
+	for _,c := range strings.Fields(s){
+		m[string(c)]+=1;
+	}
+	return m
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
