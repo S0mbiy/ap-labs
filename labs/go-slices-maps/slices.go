ef68b54fd9becb5c5a299fa91e1c1729dc081759
Sergio <A01228085@itesm.mx>
2019-09-10 21:22:28 -0500

    go maps slices exercises

--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,20 @@
 package main
 
-func main() {
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	ima := make([][]uint8, dx)
+	for x := 0; x < dx; x++ {
+		imay := make([]uint8, dy)
+		ima[x] = imay
+		for y := 0; y < dy; y++ {
+			ima[x][y]=uint8(x^y)
+		}
+	}
+	return ima
 }
+
+func main() {
+	pic.Show(Pic)
+}
+
