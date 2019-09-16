1349d66a4835181c855a443247f2eb5ca204334d
Sergio <A01228085@itesm.mx>
2019-09-16 13:57:22 -0500

    String Manipulation lib

--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,32 @@
 
 
 int mystrlen(char *str){
-    return 0;
+	int i;
+	for (i = 0; str[i] != '\0'; i++);
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+	int i;
+	int j = 0;
+	for (i = mystrlen(origin); addition[j] != '\0'; i++){
+		origin[i] = addition[j++];
+	}
+	origin[i]='\0';
+    return origin;
 }
 
 int mystrfind(char *origin, char *substr){
+	int i;
+	int j = 0;
+	for (i = 0; origin[i] != '\0'; i++){
+		if(origin[i] == substr[j++]){
+			if(j==mystrlen(substr)){
+				return 1;
+			}
+		}else{
+			j = 0;
+		}
+	}
     return 0;
 }
