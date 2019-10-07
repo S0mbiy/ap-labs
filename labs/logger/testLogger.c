533f2e4134d72d15e3dd3435b2641b5aac067e87
Sergio <A01228085@itesm.mx>
2019-10-06 19:33:42 -0500

    logger

--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,5 +1,18 @@
 
-
+int infof(const char *);
+int warnf(const char *);
+int errorf(const char *);
+int panicf(const char *);
+void setLevel(int);
 int main() {
+    setLevel(0);
+    if(infof("my info")<0)
+    return 1;
+    if(warnf("my warn")<0)
+    return 1;
+    if(errorf("my error")<0)
+    return 1;
+    if(panicf("my panic")<0)
+    return 1;
     return 0;
 }
