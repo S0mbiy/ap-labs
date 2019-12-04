0f6592bdf1aaa6eafc98b2a7561f45d770daa955
Sergio <A01228085@itesm.mx>
2019-12-03 20:38:55 -0600

    openmp

--- labs/openmp/pi_loop.c
+++ labs/openmp/pi_loop.c
@@ -1,4 +1,17 @@
+#include <omp.h>
+static long num_steps = 100000;
+double step;
+#define NUM_THREADS 2
+void main ()
+{
+	int i; double x, pi, sum = 0.0;
+	step = 1.0/(double) num_steps;
+	omp_set_num_threads(NUM_THREADS);
+	#pragma omp parallel for private(x) reduction(+:sum)
+	for (i=0;i< num_steps; i++){
+		x = (i+0.5)*step;
+		sum = sum + 4.0/(1.0+x*x);
+	}
+	pi = step * sum;
 
-int main() {
-    return 0;
 }
--- labs/openmp/pi_spmd_final.c
+++ labs/openmp/pi_spmd_final.c
@@ -1,4 +1,30 @@
+#include <omp.h>
+static long num_steps = 100000;
+double step;
+#define NUM_THREADS 2
+void main ()
+{
+	double pi;
+	double step = 1.0/(double) num_steps;
 
-int main() {
-    return 0;
+	omp_set_num_threads(NUM_THREADS);
+
+	#pragma omp parallel
+	{
+		int i, id,nthrds;
+		double x, sum;
+		id = omp_get_thread_num();
+		nthrds = omp_get_num_threads();
+		if (id == 0)
+			nthreads = nthrds;
+		id = omp_get_thread_num();
+		nthrds = omp_get_num_threads();
+		for (i=id, sum=0.0;i< num_steps; i=i+nthreads){
+			x = (i+0.5)*step;
+			sum += 4.0/(1.0+x*x);
+		}
+		#pragma omp critical
+			pi += sum * step;
+
+	}
 }
